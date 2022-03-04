6c5e9f711ff1699fc62ec5dbee60609b910ddad5
Famousmizo <hamzametalic41@gmail.com>
2022-03-04 11:13:26 +0100

    first commit create project

---
+++ app/src/main/java/com/example/baztami_app/Splash.java
@@ -0,0 +1,44 @@
+package com.example.baztami_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.WindowManager;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class Splash extends AppCompatActivity {
+
+    private static int SPLASH = 3000;
+    Animation animation;
+    private ImageView appimg;
+    private TextView appname;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        setContentView(R.layout.activity_splash);
+
+        animation = AnimationUtils.loadAnimation(this, R.anim.animation);
+        appimg = findViewById(R.id.appimg);
+        appname = findViewById(R.id.appname);
+
+        appimg.setAnimation(animation);
+        appname.setAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splash.this, Login.class);
+                startActivity(intent);
+                finish();
+            }
+        },SPLASH);
+
+    }
+}
